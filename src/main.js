a44e8e64a4172f2ad98ecce39382f39f1bd68023
darkshloser <dobromir.mail@gmail.com>
2022-02-06 19:23:02 +0200

    external links

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import routes from './router'
 
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Links from './components/ExternalLinks.vue'
 
 const app = createApp(App);
 
 app.component('app-header', Header);
 app.component('app-footer', Footer);
+app.component('app-external-links', Links);
 app.use(routes);
 app.mount('#app');
